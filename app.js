f4c9368a26f1d150d83de282ff5ca53458581807
David# <shadow2272@aol.com>
2021-07-29 22:46:32 +0100

    odin project

--- app.js
+++ app.js
@@ -1,50 +1,59 @@
-function game() {
+
   
-  let playerScore = 0
-  let cpuScore = 0
+let playerScore = 0
+let cpuScore = 0
 
-  function playOneRound() {
-    choices = ['ROCK', 'PAPER', 'SCISSORS'];
-    playerChoice = prompt('Enter selection.').toUpperCase();
-    console.log(`you choose ${playerChoice}`)
+function computerPlay() {
+  cpuChoice = Math.floor(Math.random() * choices.length);
+  cpuSelection = choices[cpuChoice];
+  console.log(cpuChoice);
+  console.log(cpuSelection);
+  return cpuSelection
+};
 
-    cpuChoice = Math.floor(Math.random() * choices.length);
-    cpuSelection = choices[cpuChoice];
-    console.log(cpuChoice);
-    console.log(cpuSelection);
+function playOneRound(p1selection, cpuSelection) {
+  choices = ['ROCK', 'PAPER', 'SCISSORS'];
+  // playerChoice = prompt('Enter selection.').toUpperCase();
+  console.log(`you choose ${p1selection}`)
 
-    if (playerChoice === cpuSelection) {
-      console.log('Draw');
-      playOneRound()
-    } else if (playerChoice === 'ROCK' && cpuSelection === 'SCISSORS') {
-      console.log('you win!');
-      ++playerScore;
-      console.log(`p1 ${playerScore} - cpu ${cpuScore}`)
-      playOneRound()
-    } else if (playerChoice === 'SCISSORS' && cpuSelection === 'PAPER') {
-      console.log('you win');
-      ++playerScore;
-      console.log(`p1 ${playerScore} - cpu ${cpuScore}`)
-      playOneRound()
-    } else if (playerChoice === 'PAPER' && cpuSelection === 'ROCK') {
-      console.log('you win');
-      ++playerScore;
-      console.log(`p1 ${playerScore} - cpu ${cpuScore}`)
-      playOneRound()
-    } else {
-      console.log('You lose');
-      ++cpuScore;
-      console.log(`p1 ${playerScore} - cpu ${cpuScore}`)
-      playOneRound()
-    }
+  
+
+
+  if (playerChoice === cpuSelection) {
+    console.log('Draw');
+    
+  } else if (playerChoice === 'ROCK' && cpuSelection === 'SCISSORS') {
+    console.log('you win!');
+    ++playerScore;
+    console.log(`p1 ${playerScore} - cpu ${cpuScore}`)
+    
+  } else if (playerChoice === 'SCISSORS' && cpuSelection === 'PAPER') {
+    console.log('you win');
+    ++playerScore;
+    console.log(`p1 ${playerScore} - cpu ${cpuScore}`)
+    
+  } else if (playerChoice === 'PAPER' && cpuSelection === 'ROCK') {
+    console.log('you win');
+    ++playerScore;
+    console.log(`p1 ${playerScore} - cpu ${cpuScore}`)
+    
+  } else {
+    console.log('You lose');
+    ++cpuScore;
+    console.log(`p1 ${playerScore} - cpu ${cpuScore}`)
+    
   }
-
-  playOneRound();
-
 }
+
+
+
+
+document.getElementById('rock').addEventListener('click', playOneRound('ROCK'));
+
+document.getElementById('scissors').addEventListener('click', playOneRound('SCISSORS'));
 
 if (playerScore >= 5) {
-  console.log('You Win 5 sets!');
+console.log('You Win 5 sets!');
 }
 
-game();
+
